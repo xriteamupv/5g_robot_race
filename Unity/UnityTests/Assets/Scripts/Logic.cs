16fc8c964cf9ad07b1d8e0242db75be913dea9b4
lauraangelespada <lauraangelespada98@gmail.com>
2023-12-12 13:42:43 +0100

    Test

--- Unity/UnityTests/Assets/Scripts/Logic.cs
+++ Unity/UnityTests/Assets/Scripts/Logic.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Logic : ScriptableObjects
+public class Logic : ScriptableObject
 {
 public string name;
 public string description; 
