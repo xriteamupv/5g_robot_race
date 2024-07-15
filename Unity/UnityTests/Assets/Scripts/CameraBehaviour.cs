7e683d4c69b1fb9de26f0a716645d702a4a63305
KevinRodriguezFivecomm <95343778+KevinRodriguezFivecomm@users.noreply.github.com>
2024-07-15 12:30:34 +0200

    Camera fixes

---
+++ Unity/UnityTests/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public Transform mainCamera;
+    public float offset;
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(mainCamera.rotation.eulerAngles.x, mainCamera.rotation.eulerAngles.y + offset, mainCamera.rotation.eulerAngles.z);
+    }
+}
