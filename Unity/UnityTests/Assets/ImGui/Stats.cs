f70e3129f37e2b2ec32e32cb308e6ed9918eb718
KevinRodriguezFivecomm <95343778+KevinRodriguezFivecomm@users.noreply.github.com>
2024-11-25 12:29:54 +0100

    Stats implementation

--- Unity/UnityTests/Assets/ImGui/Stats.cs
+++ Unity/UnityTests/Assets/ImGui/Stats.cs
@@ -3,32 +3,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.PackageManager.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Stats : MonoBehaviour
 {
-    public DataSeriesChart chart;
+    public GameObject canvas;
+    public DataSeriesChart[] chart;
+    public Text[] currentValues;
     DoubleVector3[] dataArray = new DoubleVector3[20];
     public double TotalPoints = 20;
     double x = 0;
+    CategoryDataHolder[] data;
 
     private float timer;
     void Awake()
     {
-        var data = chart.DataSource.GetCategory("dataseries-1").Data; //get the category data object 
-        data.Clear(); // clear the category
+        data = new CategoryDataHolder[chart.Length];
+        for(int i = 0; i < chart.Length; ++i)
+        {
+            data[i] = chart[i].DataSource.GetCategory("dataseries-" + (i + 1)).Data; //get the category data object 
+            data[i].Clear(); // clear the category
+        }
     }
 
     void Update()
     {
+        //Vector3 lookDirection = canvas.GetComponent<RectTransform>().position - Camera.main.transform.position;
+        //canvas.GetComponent<RectTransform>().rotation = Quaternion.LookRotation(lookDirection.normalized, Vector3.up);
         timer += Time.deltaTime;
         if(timer >= 0.5f)
         {
             timer = 0.0f;
-            var data = chart.DataSource.GetCategory("dataseries-1").Data; // get the category data
-            data.Append(x, Random.Range(0.0f, 200.0f)); // append a random point
+            for (int i = 0; i < chart.Length; ++i)
+            {
+                //Vector3 lookDirection = chart[i].GetComponent<RectTransform>().position - Camera.main.transform.position;
+                //chart[i].GetComponent<RectTransform>().rotation = Quaternion.LookRotation(lookDirection.normalized, Vector3.up);
+                float value = Random.Range(0.0f, 200.0f);
+                data[i].Append(x, value); // append a random point
+                currentValues[i].text = value.ToString();
+                if (data[i].Count > TotalPoints) // if the amount of points in the chart is larger then the maximum
+                    data[i].RemoveAllBefore(chart[i].Axis.View.HorizontalViewStart); // remove all points that are before the beginning of the view portion.
+            }
             x++; // increase current position
-            if (data.Count > TotalPoints) // if the amount of points in the chart is larger then the maximum
-                data.RemoveAllBefore(chart.Axis.View.HorizontalViewStart); // remove all points that are before the beginning of the view portion.
         }
     }
 }
--- Unity/UnityTests/Assets/Scripts/Controller.cs
+++ Unity/UnityTests/Assets/Scripts/Controller.cs
@@ -15,6 +15,22 @@ public class Controller : MonoBehaviour
     public TrafficLight trafficLight;
     public ProxyConnection proxyConnection;
 
+    public RectTransform canvas;
+    public RectTransform robotRect;
+
+    public float scaleFactor;
+
+    public struct BoundingBox
+    {
+        public float[] coords;
+        public string type;
+        public float timestamp;
+        public float detectionPercent;
+    }
+
+    public BoundingBox bb;
+    public LayerMask layerMask;
+
     void Start()
     {
         //Debug.Log("displays connected: " + Display.displays.Length);
@@ -25,6 +41,9 @@ public class Controller : MonoBehaviour
         {
             Display.displays[i].Activate();
         }
+        bb.coords = new float[] { 3070, 601, 3342, 1088 };
+        bb.type = "stop";
+        PlaceBB();
     }
 
     public void ConvertTexture(Texture texture)
@@ -48,6 +67,55 @@ public class Controller : MonoBehaviour
         }
 
     }
+
+    public void PlaceBB()
+    {
+        float sizeX = bb.coords[2] - bb.coords[0];
+        float sizeY = bb.coords[3] - bb.coords[1];
+
+        //float totalSizeX = 1920.0f;
+        //float totalSizeY = 960.0f;
+        float totalSizeX = 3840.0f;
+        float totalSizeY = 1920.0f;
+
+        Vector2 center = new Vector2(bb.coords[0] + sizeX * 0.5f, bb.coords[1] + sizeY * 0.5f);
+        Debug.Log(center);
+
+        float elevation = center.y / totalSizeY * 180.0f;
+        float azimuth = (center.x - totalSizeX * 0.5f) / totalSizeX * 360.0f;
+
+        Debug.Log("Elevation: " + elevation);
+        Debug.Log("Azimuth: " + azimuth);
+#if false
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 up = Camera.main.transform.up;
+
+        Quaternion azimuthRotation = Quaternion.Euler(0.0f, azimuth, 0.0f);
+        Quaternion elevationRotation = Quaternion.Euler(elevation, 0.0f, 0.0f);
+        Vector3 v = elevationRotation * up + azimuthRotation * forward;
+        canvas.localPosition = v * 200.0f;
+        //canvas.localPosition = new Vector3(canvas.localPosition.x * -angleX, canvas.localPosition.y * angleY, canvas.localPosition.z * 500.0f);
+#else
+        float elevationRad = elevation * Mathf.Deg2Rad;
+        float azimuthRad = azimuth * Mathf.Deg2Rad;
+        float x = Mathf.Sin(azimuthRad) * Mathf.Sin(elevationRad);
+        float y = Mathf.Cos(elevationRad);
+        float z = Mathf.Cos(azimuthRad) * Mathf.Sin(elevationRad);
+
+        Vector3 v = new Vector3(x, y, z).normalized;
+        canvas.position = (v * 500.0f);
+#endif
+
+        canvas.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, sizeX * scaleFactor);
+        canvas.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, sizeY * scaleFactor);
+
+        robotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, sizeX * scaleFactor);
+        robotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, sizeY * scaleFactor);
+
+        Vector3 cameraToCanvas = canvas.position - Camera.main.transform.position;
+        canvas.rotation = Quaternion.LookRotation(cameraToCanvas, Vector3.up);
+    }
+
 
     private void SwitchToRobotScene()
     {
