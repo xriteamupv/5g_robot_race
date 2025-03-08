531e164cf586f2d955ae2cb7a67382797f16a3ce
KevinRodriguezFivecomm <95343778+KevinRodriguezFivecomm@users.noreply.github.com>
2025-03-08 01:58:24 +0100

    Virtual Robot Lap Time

---
+++ Unity/UnityTests/Assets/Scripts/LapTimeCalculator.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LapTimeCalculator : MonoBehaviour
+{
+
+    public DateTime initialTime;
+    private DateTime finishTime;
+
+    public UIManager uiManager;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        finishTime = DateTime.Now;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Robot")
+        {
+            TimeSpan lapTime = finishTime - initialTime;
+            string lapTimeString = lapTime.ToString().Split('.')[0];
+            uiManager?.ChangeLapTime(lapTimeString);
+        }
+    }
+}
